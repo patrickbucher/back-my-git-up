804137122a800fa181493bbfc160fdff69896cf0
Patrick Bucher <patrick.bucher@mailbox.org>
2021-01-15 14:21:33 +0100

    first working version

--- backup.py
+++ backup.py
@@ -1,16 +1,46 @@
 #!/usr/bin/env python3
 
+from datetime import datetime
 import json
+import os
 import sys
 
 import requests
+from suitable import Api
 
 
 def main():
     if len(sys.argv) < 2:
-        raise ValueError(f'usage: {sys.argv[0]} [token]')
+        raise ValueError(f'usage: {sys.argv[0]} [token] [target]')
+    token, target = sys.argv[1], sys.argv[2]
 
-    print(fetch_repos(sys.argv[1]))
+    if os.path.exists(target):
+        if not os.path.isdir(target):
+            raise ValueError(f'{target} is not a directory')
+    else:
+        os.mkdir(target)
+
+    repos = fetch_repos(sys.argv[1])
+    repos = to_url_name_tuples(repos)
+
+    ansible = Api(['localhost'])
+    for (name, url) in repos:
+        dest = os.path.join(target, name)
+        print(f'cloning {url} to {dest}... ', end='')
+        start = datetime.now()
+        ansible.git(repo=url, dest=dest, accept_hostkey=True)
+        stop = datetime.now()
+        seconds = (stop - start).total_seconds()
+        print(f'done in {seconds:.2f} seconds')
+
+
+def to_url_name_tuples(repos):
+    tuples = []
+    names = [r.split('/')[-1] for r in repos]
+    urls = [f'git@github.com:{r}.git' for r in repos]
+    for name, url in zip(names, urls):
+        tuples.append((name, url))
+    return tuples
 
 
 def fetch_repos(token):
